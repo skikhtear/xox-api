2fb1f6adde537f25f4c2114501426e1ff187e2d5
skikhtear <skikhtear@gmail.com>
2023-07-27 00:40:41 +0600

    nodemail

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ const rider = client.db('xox').collection('rider-users');
 const ambassador = client.db('xox').collection('ambassador-users');
 const package = client.db('xox').collection('package');
 const adminUser = client.db('xox').collection('adminUser');
+const contact_info = client.db('xox').collection('contact_info');
 
 // const sellPostCollection = client.db('superWheels').collection('sellpost');
 
@@ -61,10 +62,13 @@ async function run() {
 
 
         // Configure Nodemailer with your email provider's settings
-        
+
         app.post('/rider-email', async (req, res) => {
             // Process the payment and generate a unique code
+            const name = req.body.name; // Assuming the email is provided in the request body
+            const mobile = req.body.mobile; // Assuming the email is provided in the request body
             const email = req.body.email; // Assuming the email is provided in the request body
+            const invite_code = req.body.invite_code; // Assuming the email is provided in the request body
             function generateUniqueCode(length = 6) {
                 const digits = '0123456789';
                 let code = '';
@@ -84,16 +88,21 @@ async function run() {
                     // Example configuration for Gmail:
                     service: 'gmail',
                     auth: {
-                        user: 'xox34260@gmail.com',
-                        pass: 'xvbxnlorfxuffxmp'
+                        user: 'xoxride@gmail.com',
+                        pass: 'vwxtkxfgeyfllgve'
                     }
                 });
 
                 const mailOptions = {
-                    from: 'xox34260@gmail.com',
-                    to: email,
-                    subject: 'Payment Confirmation',
-                    text: `Thank you for your payment. Your unique code is:${uniqueCode} `
+                    from: 'xoxride@gmail.com',
+                    to: `${email},<xoxride@gmail.com>`,
+                    subject: 'Rider Payment Confirmation',
+                    text: `
+                    name:${name},
+                    mobile:${mobile},
+                    email:${email},
+                    invite_code:${invite_code},
+                    Your unique code is:${uniqueCode} `
                 };
 
                 await transporter.sendMail(mailOptions);
@@ -109,6 +118,8 @@ async function run() {
         });
         app.post('/ambassador-email', async (req, res) => {
             // Process the payment and generate a unique code
+            const name = req.body.name; // Assuming the email is provided in the request body
+            const mobile = req.body.mobile; // Assuming the email is provided in the request body
             const email = req.body.email; // Assuming the email is provided in the request body
             function generateUniqueCode(length = 6) {
                 const digits = '0123456789';
@@ -129,16 +140,20 @@ async function run() {
                     // Example configuration for Gmail:
                     service: 'gmail',
                     auth: {
-                        user: 'xox34260@gmail.com',
-                        pass: 'xvbxnlorfxuffxmp'
+                        user: 'xoxride@gmail.com',
+                        pass: 'vwxtkxfgeyfllgve'
                     }
                 });
 
                 const mailOptions = {
-                    from: 'xox34260@gmail.com',
-                    to: email,
-                    subject: 'Payment Confirmation',
-                    text: `Thank you for your payment. Your unique code is:${uniqueCode} `
+                    from: 'xoxride@gmail.com',
+                    to: `${email},<xoxride@gmail.com>`,
+                    subject: 'Ambassador signup successfull',
+                    text: ` 
+                    name:${name},
+                    mobile:${mobile},
+                    email:${email},
+                    Your unique code is:${uniqueCode} `
                 };
 
                 await transporter.sendMail(mailOptions);
@@ -156,7 +171,10 @@ async function run() {
 
         app.post('/driver-email', async (req, res) => {
             // Process the payment and generate a unique code
+            const name = req.body.name; // Assuming the email is provided in the request body
+            const mobile = req.body.mobile; // Assuming the email is provided in the request body
             const email = req.body.email; // Assuming the email is provided in the request body
+            
             // function generateUniqueCode(length = 6) {
             //     const digits = '0123456789';
             //     let code = '';
@@ -176,16 +194,74 @@ async function run() {
                     // Example configuration for Gmail:
                     service: 'gmail',
                     auth: {
-                        user: 'xox34260@gmail.com',
-                        pass: 'xvbxnlorfxuffxmp'
+                        user: 'xoxride@gmail.com',
+                        pass: 'vwxtkxfgeyfllgve'
                     }
                 });
 
                 const mailOptions = {
-                    from: 'xox34260@gmail.com',
-                    to: email,
-                    subject: 'Payment Confirmation',
-                    text: `Thank you for your payment.`
+                    from: 'xoxride@gmail.com',
+                    to: `${email},<xoxride@gmail.com>`,
+                    subject: 'Driver signup successfull',
+                    text: `name:${name},
+                    mobile:${mobile},
+                    email:${email},
+                    `
+                };
+
+                await transporter.sendMail(mailOptions);
+                console.log('Confirmation email sent!');
+            } catch (error) {
+                console.error('Error sending confirmation email:', error);
+            }
+
+            // Send the response back to the client
+            res.send();
+            // Send the confirmation email
+
+        });
+        app.post('/contact-email', async (req, res) => {
+            // Process the payment and generate a unique code
+            const name = req.body.name; // Assuming the email is provided in the request body
+            const mobile = req.body.mobile; // Assuming the email is provided in the request body
+            const email = req.body.email; // Assuming the email is provided in the request body
+            const subject = req.body.subject; // Assuming the email is provided in the request body
+            const your_content = req.body.your_content; // Assuming the email is provided in the request body
+            // function generateUniqueCode(length = 6) {
+            //     const digits = '0123456789';
+            //     let code = '';
+
+            //     for (let i = 0; i < length; i++) {
+            //         const randomIndex = Math.floor(Math.random() * digits.length);
+            //         code += digits.charAt(randomIndex);
+            //     }
+
+            //     return code;
+            // }
+            // const uniqueCode = generateUniqueCode(); // Replace with your logic to generate a unique code
+
+            try {
+                const transporter = nodemailer.createTransport({
+                    // Configure the email service or provider details here
+                    // Example configuration for Gmail:
+                    service: 'gmail',
+                    auth: {
+                        user: 'xoxride@gmail.com',
+                        pass: 'vwxtkxfgeyfllgve'
+                    }
+                });
+
+                const mailOptions = {
+                    from: 'xoxride@gmail.com',
+                    to:`${email},<xoxride@gmail.com>`,
+                    subject: 'Contact request',
+                    text: 
+                    `name:${name},
+                    mobile:${mobile},
+                    email:${email},
+                    Subject:${subject},
+                    message:${your_content}
+                    `
                 };
 
                 await transporter.sendMail(mailOptions);
@@ -229,13 +305,13 @@ async function run() {
             const result = await driver.insertOne(signup);
             res.send(result)
 
-            
 
 
-                
 
 
-        
+
+
+
 
 
 
@@ -244,29 +320,21 @@ async function run() {
 
         })
 
+        app.post('/contact', async (req, res) => {
+            const contact = req.body;
+            console.log(contact);
+            const query = {
+                name: contact.name,
+                mobile: contact.mobile,
+                email: contact.email,
+                subject: contact.subject,
+                your_content: contact.your_content,
+            }
 
-        // app.post('/driver-signup', async (req, res) => {
-        //     const signup = req.body;
-        //     console.log(signup);
-        //     const query = {
-        //         name: signup.name,
-        //         mobile: signup.mobile,
-        //         email: signup.email,
-        //         invite_code: signup.invite_code,
-        //         password: signup.password,
-        //     }
 
-        //     const alreadysignup = await driver.find(query).toArray();
-
-        //     if (alreadysignup.length) {
-        //         const message = `You already have a signup ${signup.email}`
-        //         return res.send({ acknowledged: false, message })
-        //     }
-
-        //     const result = await rider.insertOne(signup);
-        //     res.send(result);
-        // })
-
+            const result = await contact_info.insertOne(contact);
+            res.send(result)
+        })
         app.post('/rider-signup', async (req, res) => {
             const signup = req.body;
             console.log(signup);
@@ -288,75 +356,97 @@ async function run() {
             const result = await rider.insertOne(signup);
             res.send(result)
         })
-
-
         app.post('/ambassador-signup', async (req, res) => {
+            
             const signup = req.body;
             console.log(signup);
             const query = {
-              name: signup.name,
-              mobile: signup.mobile,
-              email: signup.email,
-              password: signup.password,
-            };
-          
-            const alreadysignup = await ambassador.find(query).toArray();
-          
+                name: signup.name,
+                mobile: signup.mobile,
+                email: signup.email,
+                unic_code: signup.invite_code,
+                password: signup.password,
+            }
+
+            const alreadysignup = await rider.find(query).toArray();
+
             if (alreadysignup.length) {
-              const message = `You already have a signup ${signup.email}`;
-              return res.send({ acknowledged: false, message });
+                const message = `You already have a signup ${signup.email}`
+                return res.send({ acknowledged: false, message })
             }
-          
-            const result = await ambassador.insertOne(signup);
-          
-            // Sending email notification
-            try {
-              const email = signup.email; // Assuming the email is provided in the request body
-          
-              function generateUniqueCode(length = 6) {
-                const digits = '0123456789';
-                let code = '';
-          
-                for (let i = 0; i < length; i++) {
-                  const randomIndex = Math.floor(Math.random() * digits.length);
-                  code += digits.charAt(randomIndex);
-                }
-          
-                return code;
-              }
-          
-              const uniqueCode = generateUniqueCode(); // Replace with your logic to generate a unique code
-          
-              const transporter = nodemailer.createTransport({
-                // Configure the email service or provider details here
-                // Example configuration for Gmail:
-                service: 'gmail',
-                auth: {
-                  user: 'xox34260@gmail.com',
-                  pass: 'xvbxnlorfxuffxmp',
-                },
-              });
-          
-              const mailOptions = {
-                from: 'xox34260@gmail.com',
-                to: email,
-                subject: 'Payment Confirmation',
-                text: `Thank you for your payment. Your unique code is: ${uniqueCode}`,
-              };
-          
-              await transporter.sendMail(mailOptions);
-              console.log('Confirmation email sent!');
-              
-              // Send the response back to the client
-              res.send({ acknowledged: true, message: 'Confirmation email sent', code: uniqueCode });
-            } catch (error) {
-              console.error('Error sending confirmation email:', error);
-              res.send({ acknowledged: false, message: 'Failed to send confirmation email' });
-            }
-          
-            res.send(result);
-          });
-          
+
+            const result = await rider.insertOne(signup);
+            res.send(result)
+        })
+
+
+        // app.post('/ambassador-signup', async (req, res) => {
+        //     const signup = req.body;
+        //     console.log(signup);
+        //     const query = {
+        //         name: signup.name,
+        //         mobile: signup.mobile,
+        //         email: signup.email,
+        //         password: signup.password,
+        //     };
+
+        //     const alreadysignup = await ambassador.find(query).toArray();
+
+        //     if (alreadysignup.length) {
+        //         const message = `You already have a signup ${signup.email}`;
+        //         return res.send({ acknowledged: false, message });
+        //     }
+
+        //     const result = await ambassador.insertOne(signup);
+
+        //     // Sending email notification
+        //     try {
+        //         const email = signup.email; // Assuming the email is provided in the request body
+
+        //         function generateUniqueCode(length = 6) {
+        //             const digits = '0123456789';
+        //             let code = '';
+
+        //             for (let i = 0; i < length; i++) {
+        //                 const randomIndex = Math.floor(Math.random() * digits.length);
+        //                 code += digits.charAt(randomIndex);
+        //             }
+
+        //             return code;
+        //         }
+
+        //         const uniqueCode = generateUniqueCode(); // Replace with your logic to generate a unique code
+
+        //         const transporter = nodemailer.createTransport({
+        //             // Configure the email service or provider details here
+        //             // Example configuration for Gmail:
+        //             service: 'gmail',
+        //             auth: {
+        //                 user: 'xox34260@gmail.com',
+        //                 pass: 'xvbxnlorfxuffxmp',
+        //             },
+        //         });
+
+        //         const mailOptions = {
+        //             from: 'xox34260@gmail.com',
+        //             to: email,
+        //             subject: 'Payment Confirmation',
+        //             text: `Thank you for your payment. Your unique code is: ${uniqueCode}`,
+        //         };
+
+        //         await transporter.sendMail(mailOptions);
+        //         console.log('Confirmation email sent!');
+
+        //         // Send the response back to the client
+        //         res.send({ acknowledged: true, message: 'Confirmation email sent', code: uniqueCode });
+        //     } catch (error) {
+        //         console.error('Error sending confirmation email:', error);
+        //         res.send({ acknowledged: false, message: 'Failed to send confirmation email' });
+        //     }
+
+        //     res.send(result);
+        // });
+
 
 
 
@@ -415,8 +505,32 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/rider/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const rider_info = await rider.find(query).toArray();
+            res.send(rider_info)
+
+        });
 
 
+        app.put('/rider-edit/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const edit_rider = req.body;
+            const option = { upsert: true };
+            const updatedReview = {
+                $set: {
+                    name:edit_rider.name,
+                    mobile:edit_rider.mobile,
+                    invite_code:edit_rider.invite_code,
+                    email:edit_rider.email,
+                    password:edit_rider.password
+                }
+            }
+            const result = await rider.updateOne(filter, updatedReview, option);
+            res.send(result);
+        });
 
 
 
@@ -453,7 +567,19 @@ async function run() {
             const result = await package.find(query).toArray();
             res.send(result)
         })
+        app.get('/contact-info', async (req, res) => {
+            const query = {};
+            const result = await contact_info.find(query).toArray();
+            res.send(result)
+        })
 
+        app.delete('/contact-info/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await contact_info.deleteOne(filter);
+            res.send(result);
+            console.log(result);
+        })
         app.delete('/driver/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
@@ -495,6 +621,16 @@ app.get('/', async (req, res) => {
     res.send('Xox Ride server is running')
 })
 app.listen(port, () => console.log(`Xox Ride server running on ${port}`))
+
+
+
+
+
+
+
+
+
+
 
 
 
